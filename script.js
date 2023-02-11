b19b6099548409ff0119505e3d63116b23537363
Ashish chaurasiya <104759800+ashish9680@users.noreply.github.com>
2023-02-11 15:41:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,20 +26,7 @@ $(document).ready(function(){
 });
 
 var quotes = [
-    'It\'s all about People, Place, Money and Love of your choice!',
-    'You are what you feed your mind!',
-    'There is nothing either good or bad but your thinking makes it so!',
-    'Your actions are the results of your thoughts & your thoughts are the results of the information you feed your mind with!',
-    'Bless yourself with peace, positivity & purity!',
-    'You have the power to make your problem aggravate or diminish with the help of the thoughts you create in your mind associated with your problems.',
-    'It\'s not an end, there is always yet more to come!',
-    'अनुशासन के बिना कोई शासन नहीं कर सकता ।',
-    'Self help is the best help!',
-    'Whatever we think and speak, gets manifested.',
-    'Keep no label for yourslef, recreate yourself daily.',
-    'Strangers make the best mirrors!',
-    'What may you want, may not be what you need.',
-    'You may keep chasing what you want but you will attract only what you need.',
+    
 
     ]
     
